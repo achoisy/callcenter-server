adf50240bb7392f35610a218c8326f8263b7b362
Alexandre Choisy <alexandre.choisy@gmail.com>
2021-07-14 09:21:59 -0400

    Add call route and model

---
+++ src/models/call.ts
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+
+// An interface that describe proproties neded to create a new call
+interface CallAttrs {
+  CallSid: string;
+  ConfSid?: string;
+  AccountSid: string;
+  CallerCountry: string;
+  Direction: string;
+  CallbackSource: string;
+  CallStatus: string;
+  To: string;
+  From: string;
+  CallDuration: number;
+}
+
+interface CallDoc extends mongoose.Document {
+  CallSid: string;
+  AccountSid: string;
+  CallerCountry: string;
+  Direction: string;
+  CallbackSource: string;
+  CallStatus: string;
+  To: string;
+  From: string;
+  CallDuration: number;
+}
+
+// Interface that describe propoties a user model have
+interface CallModel extends mongoose.Model<CallDoc> {
+  build(attrs: CallAttrs): CallDoc;
+}
+
+const callSchema = new mongoose.Schema({
+  CallSid: {
+    type: String,
+  },
+  ConfSid: {
+    type: String,
+  },
+  AccountSid: {
+    type: String,
+  },
+  CallerCountry: {
+    type: String,
+  },
+  Direction: {
+    type: String,
+  },
+  CallbackSource: {
+    type: String,
+  },
+  CallStatus: {
+    type: String,
+  },
+  To: {
+    type: String,
+  },
+  From: {
+    type: String,
+  },
+  CallDuration: {
+    type: Number,
+  },
+});
+
+// Only for typescript to work properly
+callSchema.statics.build = (attrs: CallAttrs) => {
+  return new Call(attrs);
+};
+
+const Call = mongoose.model<CallDoc, CallModel>('Call', callSchema);
+
+export { Call };
---
+++ src/routes/call.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express';
+import { validateRequest, configuration, xmlHeader } from '../middlewares/';
+import {
+  BadRequestError,
+  CustomError,
+  DatabaseConnectionError,
+} from '../errors/';
+import { Call } from '../models/call';
+
+const router = express.Router();
+
+// Log incoming call webhook
+router.post('/incoming', xmlHeader, async (req, res) => {
+  console.log(req.body);
+  try {
+    const {
+      CallSid,
+      AccountSid,
+      CallerCountry,
+      Direction,
+      CallbackSource,
+      CallStatus,
+      To,
+      From,
+      CallDuration,
+    } = req.body;
+
+    // Store call in database
+    const call = Call.build({
+      CallSid,
+      AccountSid,
+      CallerCountry,
+      Direction,
+      CallbackSource,
+      CallStatus,
+      To,
+      From,
+      CallDuration,
+    });
+
+    await call.save();
+    res.status(201).send(call);
+  } catch (error) {
+    if (error instanceof CustomError) {
+      throw error;
+    }
+    throw new DatabaseConnectionError('Could not add new call');
+  }
+});
